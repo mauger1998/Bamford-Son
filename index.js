4a59c946a9aaf1d4ef7af71bd232fca8bc6765e9
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-12-01 16:02:50 +0000

    Fix all media queries, get form working and all buttons, add burger menu

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const form = document.querySelector(".scroll")
+const contactButtons = Array.from(document.querySelectorAll(".contactButton"))
+const workButton = document.querySelector(".workButton")
+const workSection = document.querySelector(".image-gallery")
 
+contactButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        form.scrollIntoView()
+    })
+    
+})
+
+workButton.addEventListener("click", (e) => {
+    workSection.scrollIntoView()
+})
 
 
